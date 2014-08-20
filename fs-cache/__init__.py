e3d421c7bd31eed4ce8eeeddc559351f9233383a
vs <vs@hosteurope.de>
2014-08-20 19:00:18 +0200

    initial add

---
+++ fs-cache/__init__.py
@@ -0,0 +1,222 @@
+'''
+Classes for salts filesystem cache for larger installations.
+'''
+import salt.utils
+
+import salt.config
+import time
+import multiprocessing
+import random
+import sys
+import zmq
+from fsworker import FSWorker
+from threading import Thread, Timer, Event
+
+
+class FSTimer(Thread):
+    '''
+    A basic timer class the fires timer-events every second.
+    '''
+    def __init__(self, event):
+        Thread.__init__(self)
+        self.stopped = event
+        self.daemon = True
+        self.serial = salt.payload.Serial('msgpack')
+
+    def run(self):
+        '''
+        main loop that fires the event every second
+        '''
+        context = zmq.Context()
+        # the socket for outgoing timer events
+        socket = context.socket(zmq.PUSH)
+        socket.setsockopt(zmq.LINGER, 100)
+        socket.bind("ipc:///tmp/fsc_timer")
+
+        count = 0
+        while not self.stopped.wait(1):
+            socket.send(self.serial.dumps(count))
+
+            count += 1
+            if count >= 60:
+                count = 0
+
+
+class FSCache(multiprocessing.Process):
+    '''
+    Provides access to the cache-system and manages the subprocesses
+    that do the cache updates in the background.
+
+    Access to the cache is available to any module that connects
+    to the FSCaches IPC-socket.
+    '''
+
+    def __init__(self, opts):
+        '''
+        starts the timer and inits the cache itself
+        '''
+        super(FSCache, self).__init__()
+        # the possible settings for the cache
+        self.opts = opts
+
+        # all jobs the FSCache should run in intervals
+        self.jobs = {}
+        # the actual cached data
+        self.path_data = {}
+
+        # the timer provides 1-second intervals to the loop in run()
+        # to make the cache system most responsive, we keep the loop-delay
+        # to 0.05s which makes it hard to get 1-second intervals
+        self.timer_stop = Event()
+        self.timer = FSTimer(self.timer_stop)
+        self.timer.start()
+
+        # the delay for the while-loop in run(), keep this as low as
+        # possible to make the cache-system most repsonsive
+        self.loop_delay = 0.05
+
+        # our serializer
+        self.serial = salt.payload.Serial("msgpack")
+
+    def add_job(self, **kwargs):
+        '''
+        adds a new job to the FSCache
+        '''
+        req_vars = ['name', 'path', 'ival', 'patt']
+
+        # make sure new jobs have all variables set
+        for var in req_vars:
+            if var not in kwargs:
+                raise AttributeError, "missing variable {0}".format(var)
+        job_name = kwargs['name']
+        del kwargs['name']
+        self.jobs[job_name] = {}
+        self.jobs[job_name].update(kwargs)
+
+    def run_job(self, name):
+        '''
+        creates a new subprocess to execute the given job in
+        '''
+        sub_p = FSWorker(**self.jobs[name])
+        sub_p.start()
+
+    def run(self):
+        '''
+        main loop of the FSCache, checks schedule, retrieves result-data
+        from the workers and answer requests with data from the cache
+        '''
+        context = zmq.Context()
+        # the socket for incoming cache requests
+        creq_in = context.socket(zmq.REP)
+        creq_in.setsockopt(zmq.LINGER, 100)
+        creq_in.bind("ipc:///tmp/fsc_cache")
+
+        # the socket for incoming cache-updates from workers
+        cupd_in = context.socket(zmq.REP)
+        cupd_in.setsockopt(zmq.LINGER, 100)
+        cupd_in.bind("ipc:///tmp/fsc_upd")
+
+        # the socket for the timer-event
+        timer_in = context.socket(zmq.PULL)
+        timer_in.setsockopt(zmq.LINGER, 100)
+        timer_in.connect("ipc:///tmp/fsc_timer")
+
+        poller = zmq.Poller()
+        poller.register(creq_in, zmq.POLLIN)
+        poller.register(cupd_in, zmq.POLLIN)
+        poller.register(timer_in, zmq.POLLIN)
+
+        while True:
+
+            # we check every 10ms for new events on any socket
+            socks = dict(poller.poll(10))
+
+            # check for next cache-request
+            if socks.get(creq_in) == zmq.POLLIN:
+                msg = self.serial.loads(creq_in.recv())
+                print "FSCACHE:  request {0}".format(msg)
+
+                # we only accept requests as lists [req_id, <path>]
+                if isinstance(msg, list):
+                    # for now only one item is assumed to be requested
+                    msgid, file_n = msg[:]
+                    print "FSCACHE:  looking for {0}:{1}".format(msgid, file_n)
+
+                    fdata = self.path_data.get(file_n, None)
+
+                    if fdata is not None:
+                        print "FSCACHE:  hit"
+                    else:
+                        print "FSCACHE:  miss"
+
+#                        randsleep = random.randint(0,3)
+#                        time.sleep(randsleep)
+                    # Send reply back to client
+                    reply = self.serial.dumps([msgid, fdata])
+                    creq_in.send(reply)
+
+                # wrong format, item not cached
+                else:
+                    reply = self.serial.dumps([msgid, None])
+                    creq_in.send(reply)
+
+            # check for next cache-update from workers
+            elif socks.get(cupd_in) == zmq.POLLIN:
+                new_c_data = self.serial.loads(cupd_in.recv())
+                # tell the worker to exit
+                cupd_in.send(self.serial.dumps('OK'))
+
+                # check if the returned data is usable
+                if not isinstance(new_c_data, dict):
+                    print "FSCACHE:  got unusable worker result"
+                    del new_c_data
+                    continue
+
+                # the workers will return differing data:
+                # 1. '{'file1': <data1>, 'file2': <data2>,...}' - a cache update
+                # 2. '{search-path: None}' -  job was not run, pre-checks failed
+                # 3. '{}' - no files found, check the pattern if defined?
+                # 4. anything else is considered malformed
+
+                if len(new_c_data) == 0:
+                    print "FSCACHE:  got empty update from worker:"
+                elif new_c_data.values()[0] is not None:
+                    print "FSCACHE:  got cache update: {0}".format(len(new_c_data))
+                    self.path_data.update(new_c_data)
+                else:
+                    print "FSCACHE:  got malformed result dict from worker"
+                print "FSCACHE:  {0} entries".format(len(self.path_data))
+
+            # check for next timer-event to start new jobs
+            elif socks.get(timer_in) == zmq.POLLIN:
+                sec_event = self.serial.loads(timer_in.recv())
+                print "FSCACHE:  event: #{0}".format(sec_event)
+
+                # loop through the jobs and start if a jobs ival matches
+                for item in self.jobs:
+                    if sec_event in self.jobs[item]['ival']:
+                        self.run_job(item)
+
+            #time.sleep(self.loop_delay)
+
+
+if __name__ == '__main__':
+
+    opts = salt.config.master_config('/etc/salt/master')
+
+    wlk = FSCache(opts)
+    # add two jobs for jobs and cache-files
+    wlk.add_job(**{
+                    'name': 'grains',
+                    'path': '/var/cache/salt/master/minions',
+                    'ival': [2,12,22],
+                    'patt': '^.*/data.p$'
+                  })
+
+    wlk.add_job(**{
+                    'name': 'mine',
+                    'path': '/var/cache/salt/master/jobs/',
+                    'ival': [4,14,24,34,44,54],
+                    'patt': '^.*$'
+                 })
+    wlk.start()
---
+++ fs-cache/fstest.py
@@ -0,0 +1,215 @@
+#!/usr/bin/python
+'''
+Classes for salts filesystem cache for larger installations.
+'''
+import salt.utils
+import salt.config
+import time
+import random
+from random import shuffle
+import sys
+import zmq
+from __init__ import FSCache
+from fsworker import Statwalker
+import argparse
+
+
+class Argparser(object):
+
+    def __init__(self):
+        self.main_parser = argparse.ArgumentParser()
+        self.add_args()
+
+    def add_args(self):
+
+        self.main_parser.add_argument('-f',
+                                      type=str,
+                                      default='',
+                                      dest='pathfile',
+                                      required=True,
+                                      help='the file from which to load paths')
+
+        self.main_parser.add_argument('-r',
+                                      type=int,
+                                      default=1,
+                                      dest='runs',
+                                      required=False,
+                                      help='the number of runs to execute')
+
+    def parse_args(self):
+        return self.main_parser.parse_args()
+
+
+class FSCacheTest(object):
+    '''
+    run different tests for the FSCache
+    '''
+
+    def __init__(self, opts, runs=1):
+        self.opts = opts
+        self.runs = runs
+        self.serial = salt.payload.Serial("msgpack")
+
+        # the cache timeout in ms * 5 is the max time we allow
+        # the cache to take to reply to the request
+        self.cache_timeout = 50
+
+        self.dirs = [
+                        '/var/cache/salt/master/jobs/',
+                        '/var/cache/salt/master/minions/'
+                    ]
+
+        # the number of random files to load
+        self.rand_files = 3000
+        self.files = []
+        self.filelist = 'filelist'
+        self.setup()
+
+        self.stats = {
+                       'req_total' : 0,
+                       'hits': 0,
+                       'misses': 0,
+                       'bytes': 0,
+                       'avg_100': 0,
+                       'avg_1000': 0,
+                       'runs': 0
+                     }
+
+    def setup(self):
+        self.context = zmq.Context()
+        self.cache_cli = self.context.socket(zmq.REQ)
+        self.cache_cli.connect('ipc:///tmp/fsc_cache')
+
+        self.poll = zmq.Poller()
+        self.poll.register(self.cache_cli, zmq.POLLIN)
+
+    def do_cache_req(self, path):
+
+        # add random id to cache-request to have a 1:1 relation
+        msgid = random.randint(10000, 20000)
+        msg = [msgid, path]
+        self.stats['req_total'] += 1
+
+        try:
+            # send a request to the FSCache. If we get to here after
+            # the cache was too slow before, the send() will fail but we try
+            # anyway going by EAFP rules.
+#            print "MAIN:  Query: {0}".format(msg)
+            self.cache_cli.send(self.serial.dumps(msg))
+        except zmq.error.ZMQError:
+            # on failure we do a dummy recv()and resend the last msg. That
+            # keeps us from having to re-init the socket on failure.
+            self.cache_cli.recv()
+            print "MAIN:  Resend: {0}:{1}".format(msgid, msg)
+            self.cache_cli.send(self.serial.dumps(msg))
+
+        to_max = self.cache_timeout*5
+        to_count = 0
+
+        while 1:
+            # we wait cache_timout*5
+            self.socks = dict(self.poll.poll(self.cache_timeout))
+            if self.socks.get(self.cache_cli) == zmq.POLLIN:
+                reply = self.serial.loads(self.cache_cli.recv())
+
+                # no reply is broken, we brake the loop
+                if not reply:
+                    break
+
+                # we expect to receive only lists with an id
+                # and the data for the requested file
+                if isinstance(msg, list):
+                    # first field must be our matching request-id
+                    if msgid == reply[0]:
+                        if reply[1] is not None:
+#                            print "MAIN:  Reply: {0}:{1} bytes".format(reply[0], len(reply[1]))
+                            self.stats['hits'] += 1
+                            self.stats['bytes'] += sys.getsizeof(reply[1])
+                        else:
+#                            print "MAIN:  Reply: {0}:miss".format(reply[0])
+                            self.stats['misses'] += 1
+                        #print ""
+                        # received reply for request, enable the
+                        # break the loop to make way for next request
+                        break
+                # None is a cache miss, we have to go to disk
+                elif msg is None:
+                    print "MAIN:  cache miss..."
+                # we should never get here
+                else:
+                    print "MAIN:  deformed packet {0}".format(reply)
+                    raise zmq.error.ZMQError, "invalid state in FSCache-communication"
+            # we wait a maximum time of cache_timeout*5, which means 100ms
+            #to_count += cache_timeout
+            to_count += 1
+            if to_count == to_max:
+                to_count = 0
+                print "MAIN:  cache was too slow, breaking\n"
+                break
+
+    def load_random(self):
+        '''
+        load filenames randomly from a filelist
+        '''
+        fhandle = open('./filelist', 'r')
+
+        while not len(self.files) >= self.rand_files:
+            for file_n in fhandle.readlines():
+                rint = random.randint(1, 1000)
+                if rint % 10 == 0:
+                    self.files.append(file_n.strip())
+                if len(self.files) >= self.rand_files:
+                    break
+                else:
+                    fhandle.seek(0)
+        shuffle(self.files)
+        print "MAIN/{0}:  loaded {1} paths-strings".format(self.stats['runs'],
+                                                           len(self.files))
+
+    def do_random(self, num):
+        '''
+        do cache requests with random paths
+        '''
+        count = 0
+        t_start = time.time()
+        for file_n in self.files[:num]:
+            self.do_cache_req(file_n)
+            count += 1
+
+        t_stop = time.time()
+        t_delta = t_stop - t_start
+
+        print "MAIN/{0}:  did {1} requests in: {2}".format(self.stats['runs'],
+                                                           count,
+                                                           t_delta)
+        return t_delta
+
+    def pstats(self):
+        print "Summary for {0} runs".format(self.stats['runs'])
+        print "Requests: {0},  Hits: {1},  Misses: {2}".format(self.stats['req_total'],
+                                                               self.stats['hits'],
+                                                               self.stats['misses'])
+
+        print "avg/100: {0},  avg/1000: {1},  bytes: {2}".format(self.stats['avg_100'] / self.stats['runs'],
+                                                                 self.stats['avg_1000'] / self.stats['runs'],
+                                                                 self.stats['bytes'])
+        print ""
+
+    def run(self):
+        for _ in range(self.runs):
+            self.load_random()
+            self.stats['avg_100'] += self.do_random(100)
+            self.stats['avg_1000'] += self.do_random(1000)
+            print "MAIN:  wait 3 seconds...\n"
+            self.stats['runs'] += 1
+            time.sleep(3)
+        self.pstats()
+
+if __name__ == '__main__':
+
+    opts = salt.config.master_config('/etc/salt/master')
+    args = vars(Argparser().parse_args())
+
+    # let the things settle and the cache
+    # populate before we enter the loop
+    test = FSCacheTest(opts, args['runs']).run()
